0a0a7d411a94ea91933e76ac87e5dfd71fe03950
brunaantunes30 <brupipoliantunes@gmail.com>
2025-04-11 14:19:30 -0300

    Merge

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,5 +9,19 @@ namespace WorkBariri2.Data
             : base(options)
         {
         }
+
+        public DbSet<Usuarios> Usuario { get; set; }
+        public DbSet<Vagas> Vaga { get; set; }
+        public DbSet<InscricaoVagas> InscricaoVaga { get; set; }
+        public DbSet<Empresas> Empresa { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Usuarios>().ToTable("Usuarios");
+            builder.Entity<Vagas>().ToTable("Vagas");
+            builder.Entity<InscricaoVagas>().ToTable("InscricaoVagas");
+            builder.Entity<Empresas>().ToTable("Empresas");
+        }
     }
 }
