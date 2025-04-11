67b4f62e4943bad6a26d6aea23228159b9c8c204
ViccariGui <guipolicarpoviccari@gmail.com>
2025-04-11 09:47:18 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WorkBariri2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
