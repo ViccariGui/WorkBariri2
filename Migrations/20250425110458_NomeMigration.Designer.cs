89c1285016150217f63071de5c4c29c0e6fde760
felipecavarossi <felipecavalheirorossi642@gmail.com>
2025-04-25 08:35:30 -0300

    nada

--- Migrations/20250425110458_NomeMigration.Designer.cs
+++ Migrations/20250425110458_NomeMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WorkBariri2.Data;
 
@@ -11,9 +12,11 @@ using WorkBariri2.Data;
 namespace WorkBariri2.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250425110458_NomeMigration")]
+    partial class NomeMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
